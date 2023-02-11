59a6090b616df9d0bfe6fc2a6bac88b05d7a631d
Edward Abboud <abboudedward@gmail.com>
2023-02-11 21:05:02 +0100

    created buttons for switching between items

--- src/rotating-elements/rotatingElements.js
+++ src/rotating-elements/rotatingElements.js
@@ -1,3 +1,24 @@
+const data = ["1", "2", "3", "4", "5", "6", "7", "8"];
+let currentIndex = 0;
+
+// Setting the initial HTML
+const parentDiv = document.getElementById("rotating-elements");
+parentDiv.innerHTML = `
+<a href="#" id="left-side">
+  <p id="left">${data[data.length - 1 - currentIndex]}</p>
+</a>
+<a href="#" id="currently-selected" class="hovered">
+  <p id="middle">${data[currentIndex]}</p>
+</a>
+<a href="#" id="right-side">
+  <p id="right">${data[currentIndex + 1]}</p>
+</a>
+`;
+
+// const leftItem = document.getElementById("left-side").children[0];
+// const middleItem = document.getElementById("middle");
+// const rightItem = document.getElementById("right");
+
 // button handling
 const nextButton = document.getElementById("next-button");
 const prevButton = document.getElementById("prev-button");
@@ -11,7 +32,6 @@ const nextHandler = () => {
   const current = document.getElementById("currently-selected");
   const right = document.getElementById("right-side");
   const left = document.getElementById("left-side");
-
   current.style =
     "width: 150px; height: 150px; font-size: 15px; filter: blur(3px); z-index: 1";
   right.style =
